7815f129eaee1d31155f557bc14b0f40bdaefa6b
Stellarnear <jeremie.chatron@gmail.com>
2018-01-16 18:26:59 +0100

    Wheel picker done !!!

--- app/src/main/java/stellarnear/aquene_dealer/Divers/WheelDicePicker.java
+++ app/src/main/java/stellarnear/aquene_dealer/Divers/WheelDicePicker.java
@@ -30,26 +30,26 @@ public class WheelDicePicker extends AppCompatActivity {
         double angle_part = 360.0/n_dice;
         double time_delay_anim=1000/n_dice;
         int dist=mC.getResources().getDimensionPixelSize(R.dimen.distance_dice_wheel);
+        final ImageButton mainDice = new ImageButton(mC);
 
         for (int i = 0; i < n_dice; i++) {
 
             ImageButton imgButton = new ImageButton(mC);
             int drawableId=mC.getResources().getIdentifier("d20_"+String.valueOf(i+1), "drawable", mC.getPackageName());
             imgButton.setImageDrawable(resize(drawableId,mC.getResources().getDimensionPixelSize(R.dimen.icon_dices_wheel_size)));
-            imgButton.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            imgButton.setBackgroundColor(mC.getColor(R.color.transparent));
+
+            setPara(imgButton);
+
             final int val_dice=i+1;
             imgButton.setOnClickListener( new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     value_selected=val_dice;
-                    changeMainDice(val_dice);
+                    changeMainDice(val_dice,mainDice);
                 }
             });
 
-
             relativeCenter.addView(imgButton);
-
 
             double angle=angle_part*i;
             int distX = (int) (dist*Math.cos(Math.toRadians(angle)));
@@ -60,28 +60,34 @@ public class WheelDicePicker extends AppCompatActivity {
 
 
         if (n_dice==20){
-            ImageButton mainDice = new ImageButton(mC);
-            mainDice.setImageDrawable(resize(R.drawable.maind20,mC.getResources().getDimensionPixelSize(R.dimen.icon_main_dices_wheel_size)));
-            mainDice.setBackgroundColor(mC.getColor(R.color.transparent));
+            mainDice.setImageDrawable(resize(R.drawable.d20_main,mC.getResources().getDimensionPixelSize(R.dimen.icon_main_dices_wheel_size)));
+            setPara(mainDice);
             relativeCenter.addView(mainDice);
         }
 
 
-
     }
 
-    private void changeMainDice(int val_dice) {
+    private void setPara(ImageButton imgButton) {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+        imgButton.setLayoutParams(layoutParams);
+        imgButton.setBackgroundColor(mC.getColor(R.color.transparent));
+    }
 
+    private void changeMainDice(int val_dice,ImageButton mainDice) {
+        int drawableId=mC.getResources().getIdentifier("d20_"+String.valueOf(val_dice), "drawable", mC.getPackageName());
+        mainDice.setImageDrawable(resize(drawableId,mC.getResources().getDimensionPixelSize(R.dimen.icon_main_dices_wheel_size)));
     }
 
 
     private Drawable resize(int imageId,int pixel_size_icon) {
-            Drawable image = mC.getDrawable(imageId);
-            Bitmap b = ((BitmapDrawable)image).getBitmap();
-
-            Bitmap bitmapResized = Bitmap.createScaledBitmap(b, pixel_size_icon, pixel_size_icon, false);
-            return new BitmapDrawable(mC.getResources(), bitmapResized);
+        Drawable image = mC.getDrawable(imageId);
+        Bitmap b = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, pixel_size_icon, pixel_size_icon, false);
+        return new BitmapDrawable(mC.getResources(), bitmapResized);
     }
+
 
 
     public int getValue_selected(){
